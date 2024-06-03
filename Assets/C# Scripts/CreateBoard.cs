939a13a28b776e04b572565c2d5f40b9947f8d8e
Carlos Ocampo <118745644+SenorCarlito1@users.noreply.github.com>
2024-06-03 15:03:28 -0400

    Added create board script

---
+++ Assets/C# Scripts/CreateBoard.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateBoard : MonoBehaviour
+{
+
+    public GameObject[] board;
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Create chess board
+        
+        Sprite sprite = GetComponent<Sprite>();
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
