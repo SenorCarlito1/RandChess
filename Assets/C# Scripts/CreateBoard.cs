77f68d21359a289f45ccfb45089b7019ff5f3194
Carlos Ocampo <118745644+SenorCarlito1@users.noreply.github.com>
2024-06-03 15:50:28 -0400

    Created two sprites

--- Assets/C# Scripts/CreateBoard.cs
+++ Assets/C# Scripts/CreateBoard.cs
@@ -5,20 +5,19 @@ using UnityEngine;
 public class CreateBoard : MonoBehaviour
 {
 
-    public GameObject[] board;
+
     // Start is called before the first frame update
     void Start()
     {
-        // Create chess board
-        
         Sprite sprite = GetComponent<Sprite>();
-
         
+
     }
+
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
