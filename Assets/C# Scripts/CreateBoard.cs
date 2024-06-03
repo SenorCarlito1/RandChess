63bc4566926d36c29a331694b906e09ada78a085
Carlos Ocampo <118745644+SenorCarlito1@users.noreply.github.com>
2024-06-03 16:33:39 -0400

    Spawned square in

--- Assets/C# Scripts/CreateBoard.cs
+++ Assets/C# Scripts/CreateBoard.cs
@@ -4,13 +4,20 @@ using UnityEngine;
 
 public class CreateBoard : MonoBehaviour
 {
-
-
+    private int rand;
+    public Sprite WhiteSprite;
+    public SpriteRenderer test;
     // Start is called before the first frame update
     void Start()
     {
-        Sprite sprite = GetComponent<Sprite>();
-        
+        //rand = Random.Range(0, sprite);
+        GetComponent<SpriteRenderer>().sprite = WhiteSprite;
+        //for (int i = 0; i < 8; i++)
+        //{
+           
+        GetComponent<SpriteRenderer>().transform.localPosition = new Vector3(2f, 3f, 0f);
+        //}
+
 
     }
 
